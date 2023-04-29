d10b1d61dd8eb2d840264093ea99a58868c86779
michaelgrosen <132148249+michaelgrosen@users.noreply.github.com>
2023-04-29 15:04:42 -0400

    Update pklbot.py

--- pklbot.py
+++ pklbot.py
@@ -32,6 +32,33 @@ web.get('https://clients.mindbodyonline.com/asp/adm/adm_appt_search.asp?studioid
 
 time.sleep(2)
 
+SignIn = web.find_element(By.XPATH, '/html/body/div[3]/div[3]/div/div/div[2]/div/div[4]/div[1]/input')
+SignIn.click()
+
+time.sleep(5)
+
+Email = "mrosen6@tulane.edu"
+emailinput = web.find_element(By.XPATH, '/html/body/div/span/div/div/div/main/div/div/form/main/fieldset[1]/div/input')
+emailinput.send_keys(Email)
+
+time.sleep(5)
+
+Password = "*Stew733"
+passwordinput = web.find_element(By.XPATH, '/html/body/div/span/div/div/div/main/div/div/form/main/fieldset[2]/div/div/input')
+passwordinput.send_keys(Password)
+
+time.sleep(5)
+
+SignInButton = web.find_element(By.XPATH, '/html/body/div/span/div/div/div/main/div/div/form/section[2]/button/span')
+SignInButton.click()
+
+time.sleep(25)
+
+PickleballCourtRentals = web.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[1]/div[4]/div/div/table/tbody/tr/td[2]/table/tbody/tr/td[2]')
+PickleballCourtRentals.click()
+
+time.sleep(5)
+
 SelectThSNPeak = web.find_element(By.XPATH, '/html/body/div[2]/div[5]/div/div/div[2]/div/form/div/div[4]/div[2]/table/tbody/tr[1]/td/input')
 SelectThSNPeak.click()
 
@@ -75,9 +102,9 @@ time.sleep(1)
 SelectTime = web.find_element(By.XPATH, '/html/body/div[3]/div[3]/div/div/div[2]/div/div[6]/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[8]/td[3]/a')
 SelectTime.click()
 
-time.sleep(2)
+time.sleep(5)
 
-BookAppointment = web.find_element(By.XPATH, '/html/body/div[3]/div[3]/div/div/div[2]/div/div[6]/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[8]/td[3]/a')
+BookAppointment = web.find_element(By.XPATH, '/html/body/div[2]/div[5]/div/div/div[2]/div/div/div[2]/div/form/div/input[1]')
 BookAppointment.click()
 
 time.sleep(2)
@@ -85,17 +112,17 @@ time.sleep(2)
 PurchaseItem = web.find_element(By.XPATH, '/html/body/div[3]/div[5]/div/div/div[2]/div/div[1]/form/div/div[2]/div/div/div/ul/li/div[1]/div')
 PurchaseItem.click()
 
-time.sleep(2)
+time.sleep(15)
 
 CheckOut = web.find_element(By.XPATH, '/html/body/div[3]/div[5]/div/div/div[2]/div/div[1]/form/div/div[2]/div[3]/div/input[3]')
 CheckOut.click()
 
-time.sleep(2)
+time.sleep(7)
 
-PlaceOrder = web.find_element(By.XPATH, '/html/body/div[3]/div[5]/div/div/div[2]/div/div[1]/form/div/div[2]/div[3]/div/input[3]')
-PlaceOrder.click()
+####PlaceOrder = web.find_element(By.XPATH, '/html/body/div[3]/div[5]/div/div/div[2]/div/div[1]/form/div/div[2]/div[3]/div/input[3]')
+####PlaceOrder.click()
 
-time.sleep(2)
+time.sleep(7)
 
 #########
 
@@ -123,114 +150,74 @@ elif TimeSlot5.is_enabled():
     TimeSlot5.click()
 else:
     print("g-dangit")
-
-
 time.sleep(2)
-
 SelectGuest = web.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/main/div/div/div[1]/div[4]/div[2]')
 SelectGuest.click()
-
 time.sleep(2)
-
 FirstName = "Michael"
 first = web.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/main/div/div/div[1]/div[2]/div/div[2]/form/div[1]/div[1]/div/input')
 first.send_keys(FirstName)
-
 time.sleep(1)
-
 LastName = "Rosen"
 last = web.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/main/div/div/div[1]/div[2]/div/div[2]/form/div[1]/div[2]/div/input')
 last.send_keys(LastName)
-
 time.sleep(1)
-
 EmailAddress = "mrosen6@tulane.edu"
 email = web.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/main/div/div/div[1]/div[2]/div/div[2]/form/div[2]/div/input')
 email.send_keys(EmailAddress)
-
 time.sleep(1)
-
 PhoneNumber = "7818203654"
 phone = web.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/main/div/div/div[1]/div[2]/div/div[2]/form/div[3]/div/div/div[2]/div/input')
 phone.send_keys(PhoneNumber)
-
 time.sleep(1)
-
 FirstNameCredit = "Michael"
 firstcredit = web.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/main/div/div/div[1]/div[4]/div/div[2]/div[1]/div/div[1]/div/input')
 firstcredit.send_keys(FirstNameCredit)
-
 time.sleep(1)
-
 LastNameCredit = "Rosen"
 lastcredit = web.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/main/div/div/div[1]/div[4]/div/div[2]/div[1]/div/div[2]/div/input')
 lastcredit.send_keys(LastNameCredit)
-
 time.sleep(1)
-
 CardNumber = ""
 number = web.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/main/div/div/div[1]/div[4]/div/div[2]/div[2]/div/div/iframe')
 number.send_keys(CardNumber)
-
 time.sleep(1)
-
 MMYY = ""
 my = web.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/main/div/div/div[1]/div[4]/div/div[2]/div[2]/div/div/iframe')
 my.send_keys(MMYY)
-
 time.sleep(1)
-
 CVC = ""
 cvclocation = web.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/main/div/div/div[1]/div[4]/div/div[2]/div[2]/div/div/iframe')
 cvclocation.send_keys(CVC)
-
 time.sleep(1)
-
 ZIP = ""
 ziplocation = web.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/main/div/div/div[1]/div[4]/div/div[2]/div[2]/div/div/iframe')
 ziplocation.send_keys(ZIP)
-
 time.sleep(1)
-
 html = web.find_element(By.TAG_NAME, 'html')
 html.send_keys(Keys.END)
-
 time.sleep(1)
-
 Cancellation_Policy_Click = web.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/main/div/div/div[1]/div[5]/div/div[2]/div[8]/div[1]/button[1]')
 Cancellation_Policy_Click.click()
-
 time.sleep(1)
-
 I_Agree_To_The_Venues_Required_Policy = web.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/main/div/div/div[1]/div[5]/div/div[2]/div[8]/div[2]/button[1]')
 I_Agree_To_The_Venues_Required_Policy.click()
-
 time.sleep(1)
-
 I_Certify_I_Am_Above_The_Age_Of_18 = web.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/main/div/div/div[1]/div[5]/div/div[2]/div[8]/div[3]/button')
 I_Certify_I_Am_Above_The_Age_Of_18.click()
-
 time.sleep(1)
-
 Receive_News = web.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/main/div/div/div[1]/div[5]/div/div[2]/div[8]/div[4]/button[1]')
 Receive_News.click()
-
 time.sleep(1)
-
 Reminders = web.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/main/div/div/div[1]/div[5]/div/div[2]/div[8]/div[5]/button')
 Reminders.click()
-
 time.sleep(1)
-
 ###BigRedButton = web.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/main/div/div/div[1]/div[8]/button')
 ####BigRedButton.click()
-
 ####time.sleep(20)
-
     
 ###schedule.every().day.at("21:27:00").do(run_picklebot)
     
-
 ####while True:
     ####schedule.run_pending()
-    ####time.sleep(1)""""
+    ####time.sleep(1)"""
