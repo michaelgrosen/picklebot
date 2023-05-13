a10bba38498bc5acb2cad21a7b9b7d1fc64850b0
michaelgrosen <132148249+michaelgrosen@users.noreply.github.com>
2023-05-13 11:49:04 -0400

    Update pklbot.py

--- pklbot.py
+++ pklbot.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 import time
 import schedule
 import os
